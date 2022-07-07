e49f903ad9402a30861d877b85cfa6f8fde7dcdb
Felix <gabritelos@hotmail.com>
2022-07-07 01:00:20 -0300

    added sum

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const botoes = document.querySelectorAll(".bttn");
 const display = document.querySelector(".display");
+let acaoArmazenada = "";
+let temp = 0;
+let temp2 = 0;
 
 botoes.forEach((elementoBotao) => {
   elementoBotao.addEventListener("click", (evento) => {
@@ -19,20 +22,18 @@ function apertouBotao(simbolo) {
     case "8":
     case "9":
     case "0":
-      console.log(simbolo);
       poeNumero(simbolo);
       break;
     case "/":
-      console.log("divisao");
+      //   console.log("divisao");
       break;
     case "x":
-      console.log("multiplicacao");
+      //   console.log("multiplicacao");
       break;
     case "+":
-      console.log("adicao");
-      break;
     case "-":
-      console.log("subtracao");
+    case "=":
+      processaAcao(simbolo);
       break;
     case ".":
       console.log("ponto");
@@ -40,17 +41,71 @@ function apertouBotao(simbolo) {
     case "C":
       console.log("clear");
       limpaDisplay();
-      break;
-    case "=":
-      console.log("igual");
+      limpaTemps();
       break;
   }
 }
 function poeNumero(x) {
-  console.log(display.innerHTML);
+  //   console.log(display.innerHTML);
   display.innerHTML = display.innerHTML + x;
-  console.log(display.innerHTML);
+  //   console.log(display.innerHTML);
 }
 function limpaDisplay() {
   display.innerHTML = "";
+}
+function processaAcao(acaoPassada) {
+  if (acaoPassada != "=") {
+    console.log("acaoPassada nao eh igual");
+    //aqui a  acaoPassada eh / * - +
+    if (acaoArmazenada == "") {
+      console.log("Primeira Chamada");
+      //se primeira armazena N A
+      temp = parseFloat(display.innerHTML);
+      acaoArmazenada = acaoPassada;
+      limpaDisplay(); //prox entrada de N
+    } else {
+      console.log("nao eh a primeira vez");
+      //se nao primeira
+      //tem N e A armazenados e um novo N
+      temp2 = parseFloat(display.innerHTML); //pega novo N
+      temp = calculadora(temp, temp2, acaoArmazenada); //calcula N a Novo N
+      limpaDisplay(); //limpa novo N
+      mostraValor(temp); //mostra NAN
+      limpaDisplay(); //limpa NAN
+      //tenho NAN e A novo acaoPassada
+      acaoArmazenada = acaoPassada;
+    }
+  } else {
+    console.log("eh =");
+    temp2 = parseFloat(display.innerHTML);
+    if (!temp) {
+      mostraValor("Erro");
+      setInterval(limpaDisplay, 2000);
+      limpaTemps();
+    } else {
+      console.log("temp");
+      temp = calculadora(temp, temp2, acaoArmazenada);
+      mostraValor(temp);
+    }
+    acaoArmazenada = "";
+  }
+}
+function calculadora(x, y, acao) {
+  switch (acao) {
+    case "+":
+      return x + y;
+    case "-":
+      return x - y;
+    default:
+      console.log("default da calculadora");
+      break;
+  }
+}
+function mostraValor(valor) {
+  display.innerHTML = valor;
+}
+function limpaTemps() {
+  temp = 0;
+  temp2 = 0;
+  acaoArmazenada = "";
 }
